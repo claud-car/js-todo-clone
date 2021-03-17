61521696e76782383d27e0e71819966bfc293db7
Claudio <claudiocarchietti@yahoo.it>
2021-03-17 17:57:26 +0100

    esercizio terminato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,20 @@
 $(document).ready(function () {
-    $('.btn').click(function () {
-      var addText = $('.addtext').val();
-      $('.add-text').val('');
-
-      var addedText = $('#to-do li').clone();
-      addedText.children('span.title').text(addText);
-
-      addedText.children('span.delete').click(function () {
-        $(this).parent().remove();
-      });
-      // addedText.children('span.edit').click(function() {
-      //   var titleDOM = $(this).siblings('span.title');
-      //   var title = titleDOM.text();
-      //   titleDOM.hide();
-      //
-      //   $(this).siblings('input').val(title).show();
-      // });
+  $('.btn').click(function () {
+    var addText = $('.addtext').val();
+    $('.addtext').val('');
 
 
 
+    var template = $('#to-do li').clone();
+    template.children('span.title').text(addText);
 
-
+    template.children('span.delete').click(function() {
+      $(this).parent().remove();
     });
 
-    console.log(addText);
-    console.log(addedText);
-    console.log($(this));
 
-
+    $('#list').append(template);
+  });
 }); //end document
 
 // Consegna
@@ -36,3 +23,28 @@ $(document).ready(function () {
 // Rendiamo il tutto gradevole alla vista.
 // Bonus
 // Permettiamo la modifica di un todo già aggiunto.
+
+
+// var addText = $('.addtext').val();
+// $('.add-text').val('');
+//
+// var addedText = $('#to-do li');
+// addedText.children('span.title').text(addText);
+//
+//
+// $('span.delete').click(function () {
+//   $(this).parent().remove();
+// })
+//
+// addedText.children('span.edit').click(function() {
+//   var titleDOM = $(this).siblings('span.title');
+//   var title = titleDOM.text();
+//   titleDOM.hide();
+//
+//   $(this).siblings('input').val(title).show();
+// });
+//
+//
+//
+//
+// $('#prova').append(addedText);
